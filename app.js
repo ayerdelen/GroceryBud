21c65d0133189a3772f88126afb9d5b890cb8280
Aycan <aycanyerdelen@gmail.com>
2021-04-20 20:06:03 +0300

    project updated

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ let editID = "";
 // ****** EVENT LISTENERS **********
 //submit form
 form.addEventListener("submit", addItem);
+// clear items
+clearBtn.addEventListener("click", clearItems);
 
 // ****** FUNCTIONS **********
 function addItem(e) {
@@ -22,14 +24,88 @@ function addItem(e) {
   const value = grocery.value;
   const id = new Date().getTime().toString();
   if (value && !editFlag) {
-    console.log("add item to the list");
+    let html = `
+    <article class="grocery-item" data-id="${id}">
+    <p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+  </article>
+    `;
+    list.innerHTML += html;
+    const deleteBtn = list.querySelector(".delete-btn");
+    const editBtn = list.querySelector(".edit-btn");
+
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
+
+    //display alert
+    displayAlert("item added to the list", "success");
+    //show container
+    container.classList.add("show-container");
+    // add to local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
   } else if (value && editFlag) {
     console.log("editing");
   } else {
-    console.log("empty value");
+    displayAlert("please enter value", "danger");
   }
+}
+//display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  //remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+// clear items
+
+function clearItems() {
+  const items = document.querySelectorAll(".grocery-item");
+
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item);
+    });
+  }
+  container.classList.remove("show-container");
+  displayAlert("empty list", "danger");
+  setBackToDefault();
+  //localStorage.removeItem("list")
+}
+
+//delete function
+function deleteItem() {
+  console.log("deleted");
+}
+
+// edit function
+function editItem() {
+  console.log("edited");
+}
+
+// set back to default
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "submit";
 }
 
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {
+  console.log("added to local storage");
+}
 // ****** SETUP ITEMS **********
